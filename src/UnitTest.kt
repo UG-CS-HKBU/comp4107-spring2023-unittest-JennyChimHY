bcee906b4ffafde1375c95873b42e0b2c97b3075
JennyChimHY <chimhoiyan@gmail.com>
2023-03-22 15:21:22 +0800

    Lab13

---
+++ src/UnitTest.kt
@@ -0,0 +1,85 @@
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class UnitTest {
+//    @Test
+//    fun test1() { //ascending order, create monarch
+//        heroes.clear()
+//        monarchHero = CaoCao()
+//        heroes.add(monarchHero)
+//        heroes.add(ZhangFei(MinisterRole()))
+//        assertTrue(monarchHero.name == "Cao Cao")
+//    }
+//
+//
+//    @Test
+//    fun test2() { //create non-monarch
+//        if(heroes.size < 2) {
+//            test1()
+//        }
+//        assertTrue(heroes.size == 2)
+//    }
+
+    @Test
+    fun testCaoDodgeAttack() { //Test Doubles - Stub
+        monarchHero = CaoCao()
+        for (i in 0..6) {
+            heroes.add(NonMonarchFactory.createRandomHero())
+        }
+        play()
+        assertTrue(monarchHero.dodgeAttack()) //CaoCao dodgeAttack
+    }
+
+    @Test
+    fun testBeingAttacked() { //Test Doubles - Spy
+        if(heroes == null) {
+            heroes.add(NonMonarchFactory.createRandomHero())
+        }
+        val hero = NonMonarchFactory.createRandomHero()
+        val spy = object: WarriorHero(MinisterRole()) {
+            override val name = hero.name
+            override fun beingAttacked() {
+                hero.beingAttacked()
+                assertTrue(hero.hp >= 0)
+            }
+        }
+        for(i in 0..10)
+            spy.beingAttacked()
+    }
+
+    object FakeFactory: GameObjectFactory {
+        var count = 0 //for SimaYi, XuChu and XiaHouyuan
+        var last: WeiHero? = null
+        init {
+            monarchHero = CaoCao()
+        }
+        override fun getRandomRole(): Role =
+            MinisterRole()
+        override fun createRandomHero(): Hero {
+            val hero = when(count++) {
+                0->SimaYi(getRandomRole())
+                1->XuChu(getRandomRole())
+                else->XiaHouyuan(getRandomRole())
+            }
+            val cao = monarchHero as CaoCao
+            if (last == null)
+                cao.helper = hero
+            else
+                last!!.setNext(hero)
+            last = hero
+            return hero
+        }
+    }
+
+    class DummyRole : Role {
+        override val roleTitle = "Dummy"
+        override fun getEnemy() = "Dummy"
+    }
+
+    @Test
+    fun testDiscardCards() { //Dummy
+        val dummy = DummyRole()
+        val hero = ZhangFei(dummy)
+        hero.discardCards()
+    }
+}
